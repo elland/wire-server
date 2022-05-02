1058fbf30e6c60e486095844670472bd4c13f825
Paolo Capriotti <paolo@capriotti.io>
2022-05-02 13:32:56 +0200

    Fix integration test assertion

--- services/galley/test/integration/API.hs
+++ services/galley/test/integration/API.hs
@@ -2570,7 +2570,7 @@ testAddRemoteMemberInvalidDomain = do
   postQualifiedMembers alice (remoteBob :| []) convId
     !!! do
       const 422 === statusCode
-      const (Just "/federation/on-conversation-updated")
+      const (Just "/federation/api-version")
         === preview (ix "data" . ix "path") . responseJsonUnsafe @Value
       const (Just "invalid.example.com")
         === preview (ix "data" . ix "domain") . responseJsonUnsafe @Value
